2eba151561fc7a93916e2dcea636e17983457d4f
LucianoBampa <lucbam2@gmail.com>
2025-08-03 18:34:57 -0300

    Criando as URLs e as Views

--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+
+class ListaProdutos(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse("Lista de Produtos")
+
+
+class DetalheProduto(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse("Detalhe do Produto")
+
+
+class AdicionarAoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse("Adicionar ao Carrinho")
+
+
+class RemoverDoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse("Remover do Carrinho")
+
+
+class Carrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse("Carrinho de Compras")
+
+
+class Finalizar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse("Finalizar Compra")
